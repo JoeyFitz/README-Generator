7800eea07c35f261a813cbb6db4470ec62735cc3
Joe Fitzgerald <jfitzgerald@veritone.com>
2021-08-08 22:23:59 -0700

    updated questions and fixed markdown generation

--- README.md
+++ README.md
@@ -18,8 +18,7 @@ blah
 blah
 
 ## Licensing
-Apache license 2.0
-[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+[![Apache License 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 
 ## Contributing
 blah
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// Packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
+let licenseInfo;
+
 const questions = [
     {
         type: 'input',
@@ -16,7 +16,7 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'How do you install this repo?',
+        message: 'How does one install this repo?',
         name: 'installation',
     },
     {
@@ -30,35 +30,35 @@ const questions = [
         name: 'licensing',
         choices: 
             ['None',
-            'Apache license 2.0', 
-            'Boost Software License 1.0',
-            'BSD 2-Clause License',
-            'BSD 3-Clause License',
-            "Creative Commons Zero",
-            "Creative Commons Attribution 4.0 International",
-            "Creative Commons Attribution-ShareAlike 4.0 International",
-            "Attribution-NonCommercial 4.0 International",
-            "Attribution-NoDerivatives 4.0 International",
-            "Attribution-NonCommercial-ShareAlike 4.0 International",
-            "Attribution-NonCommercial-NoDerivatives 4.0 International",
-            "Eclipse Public License 1.0",
-            "GNU Affero General Public License v3.0",
-            "GNU General Public License v2.0",
-            "GNU General Public License v3.0",
-            "GNU Lesser General Public License v3.0",
-            "IBM Public License Version 1.0",
-            "ISC License",
-            "The MIT License",
-            "Mozilla Public License 2.0",
-            "Attribution License (BY)",
-            "Open Database License (ODbL)",
-            "Public Domain Dedication and License (PDDL)",
-            "The Perl License",
-            "The Artistic License 2.0",
-            "SIL Open Font License 1.1",
-            "The Unlicense",
-            "The Do What The Fuck You Want To Public License",
-            "The zLib/libpng License"],
+            '[![Apache License 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)', 
+            '[![Boost Software License 1.0](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)',
+            '[![BSD 2-Clause License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)',
+            '[![BSD 3-Clause License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
+            '[![CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)',
+            '[![CC BY 4.0](https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)',
+            '[![CC BY-SA 4.0](https://img.shields.io/badge/License-CC%20BY--SA%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by-sa/4.0/)',
+            '[![CC BY-NC 4.0](https://img.shields.io/badge/License-CC%20BY--NC%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc/4.0/)',
+            '[![CC BY-ND 4.0](https://img.shields.io/badge/License-CC%20BY--ND%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nd/4.0/)',
+            '[![CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)',
+            '[![CC BY-NC-ND 4.0](https://img.shields.io/badge/License-CC%20BY--NC--ND%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-nd/4.0/)',
+            '[![Eclipse Public License 1.0](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)',
+            '[![GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)',
+            '[![GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+            '[![AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)',
+            '[![LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)',
+            '[![FDL 1.3](https://img.shields.io/badge/License-FDL%20v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)',
+            '[![IBM Public License 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)',
+            '[![ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)',
+            '[![The MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+            '[![Mozilla Public License 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)',
+            '[![Open Data Commons Attribution License](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)](https://opendatacommons.org/licenses/by/)',
+            '[![Open Database License](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)',
+            '[![Public Domain Dedication License](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](https://opendatacommons.org/licenses/pddl/)',
+            '[![The Perl License](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)',
+            '[![SIL Open Font License 1.1](https://img.shields.io/badge/License-OFL%201.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)',
+            '[![The Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)',
+            '[![The Do What the Fuck You Want to Public License](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)',
+            '[![Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)'],
     },
     {
         type: 'input',
@@ -81,12 +81,6 @@ const questions = [
         name: 'github',
         },
 ];
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
 
 inquirer
   .prompt(questions)
@@ -114,7 +108,6 @@ ${response.usage}
 
 ## Licensing
 ${response.licensing}
-[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 
 ## Contributing
 ${response.contributing}
@@ -126,13 +119,5 @@ ${response.tests}
 Please follow my work at ${response.github} and reach out with any questions - ${response.email}`;
 
     fs.writeFile('README.md', readMeTemplate, (err) => err ? console.log(err): console.log('Success!') );
-    // renderLicenseBadge(response.licensing);
-  }
+    }
   );
-
-// function renderLicenseBadge () {
-//     if (response.licensing = 'Apache license 2.0'){
-//         let licenseInfo = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
-//         licenseInfo
-//     }
-// }
